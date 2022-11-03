a234b032b359a170d2789774d7e457bf6f398cdb
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2022-11-03 11:18:33 -0700

    add prelim navbar (#3)
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,40 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 
 import { Link } from "react-router-dom";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
-import '../App.css';
+import logo from "../imgs/girlcode.svg";
+
+import "../App.css";
 
 export default function NavBar() {
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" className="nav-bar">
+      <AppBar position="static" className="navbar">
+        <img src={logo} alt="ubc girlcode" className="navbar-logo"></img>
         <Toolbar>
-            <Link to="/about" className="nav-link">
-            About
+          <Box sx={{ flexGrow: 1 }}>
+            <Link to="/about" className="social-link">
+              <FaInstagram size={25} className="social" />
             </Link>
-            <Link to="/sign-up" className="nav-link">
-              Sign Up
+            <Link to="/about" className="social-link">
+              <FaFacebook size={25} className="social" />
             </Link>
             <Link to="/contact" className="nav-link">
               Contact
             </Link>
+            <Link to="/sign-up" className="nav-link">
+              Register
+            </Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </Box>
         </Toolbar>
       </AppBar>
-    </Box>
     </>
   );
 }
-
-
