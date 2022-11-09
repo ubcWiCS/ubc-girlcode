4bedfc79c185003ce63cab06e4169033c3979aaa
ubcWiCS <89810740+ubcWiCS@users.noreply.github.com>
2022-11-08 18:05:29 -0800

    Nav style update (#6)
    * added in homepage content without background bubbles
    
    * made homepage responsive
    
    * embedded qulatrics form
    
    * updated the styling for the nav bar (made logo less shrinky, adjusted font and colours to match current design)
    
    git status
    
    * adjust register title alignment
    
    Co-authored-by: Lena Wang <xianna03@gmail.com>
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

--- src/components/SocialSideBar.jsx
+++ src/components/SocialSideBar.jsx
@@ -1,23 +1,20 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 
 import { FaInstagram, FaFacebook, FaTiktok, FaLinkedin } from "react-icons/fa";
 
 import "../App.css";
 
-export default function NavBar() {
+export default function SocialSideBar(props) {
   return (
     <>
-      <AppBar position="absolute" className="socialSideBar">
-          <div className = "icons">
-            <FaInstagram size={25} className="social vertical" />
-            <FaTiktok size={25} className="social vertical" />
-            <a href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D">
-              <FaFacebook size={25} className="social vertical" />
-            </a>
-            <FaLinkedin size={25} className="social vertical" />
-          </div>
-      </AppBar>
+      <div className={props.className}>
+        <FaInstagram size={25} className="social vertical" />
+        <FaTiktok size={25} className="social vertical" />
+        <a href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D">
+          <FaFacebook size={25} className="social vertical" />
+        </a>
+        <FaLinkedin size={25} className="social vertical" />
+      </div>
     </>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import SocialSideBar from "../components/SocialSideBar";
+import register from "../imgs/registerText.svg";
 
 export default function SignUp() {
   return (
     <>
-        <NavBar />
-        <p>SignUp</p>
-        <Footer />
+      <NavBar />
+      <div className="regPageTitle">
+        <div>
+          <img src={register} alt="register"></img>
+        </div>
+        <div className="regPageSocials">
+        <SocialSideBar className="reg-icons" />
+        </div>
+      </div>
+      <div className="registerPageBody">
+        <iframe
+          title="qualtrics"
+          src="https://ubc.ca1.qualtrics.com/jfe/form/SV_bO6taMrJnPF81kG"
+          width="800px"
+          height="3000px"
+          frameBorder="0"
+        ></iframe>
+      </div>
+      <Footer />
     </>
   );
 }
-
