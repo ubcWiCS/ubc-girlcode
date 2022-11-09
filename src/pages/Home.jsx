8f6a0ada86a4a17175ec38299fc35afca84d6659
Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
2022-11-08 18:03:08 -0800

    adjust register title alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,48 +2,73 @@ import * as React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import SocialSideBar from "../components/SocialSideBar"
+import SocialSideBar from "../components/SocialSideBar";
 import girlCode from "../imgs/girlCodeText.svg";
-
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <div className= "landingPage">
-        
+      <div className="landingPage">
         <img src={girlCode} alt="girlcode"></img>
-        <SocialSideBar />
+        <SocialSideBar className="icons" />
       </div>
-      <div className = "content">
-        <div className = "leftJustified">
-        <paragraph><purple>Women in Computer Science</purple> and <blue>Women in Data Science</blue> are proud to present UBC's 
-          first fashion-themed hackathon for underrepresented gender minorities. <br/> <br/>  #GirlsGitIt 
-          <br/><br/><br/>
-          <button1><a href="/about">Learn More</a></button1>
-        </paragraph>
+      <div className="content">
+        <div className="leftJustified">
+          <paragraph>
+            <purple>Women in Computer Science</purple> and{" "}
+            <blue>Women in Data Science</blue> are proud to present UBC's first
+            fashion-themed hackathon for underrepresented gender minorities.{" "}
+            <br /> <br /> #GirlsGitIt
+            <br />
+            <br />
+            <br />
+            <button1>
+              <a href="/about">Learn More</a>
+            </button1>
+          </paragraph>
         </div>
-          <div className = "rightJustified">
+        <div className="rightJustified">
           <gradientFont>24</gradientFont>
-          <br/> 
-          <paragraph>hour <bold>in-person</bold> hackathon <br/> <br/> Hosted at <bold>UBC</bold>, from January 13th-14th 2023. <br/> Save the date and register today.</paragraph>
+          <br />
+          <paragraph>
+            hour <bold>in-person</bold> hackathon <br /> <br /> Hosted at{" "}
+            <bold>UBC</bold>, from January 13th-14th 2023. <br /> Save the date
+            and register today.
+          </paragraph>
         </div>
-        <div className = "centre">
-          <div className = "glass">
-            <paragraph>girlCode strives to <bold>ignite</bold> creativity, <bold>build</bold> friendships and <bold>empower</bold> the next generation of leaders in tech through building impactful solutions. </paragraph>
-            <br/><br/><br/><br/>
-            <button1><a href="/sign-up">Register Now</a></button1>
+        <div className="centre">
+          <div className="glass">
+            <paragraph>
+              girlCode strives to <bold>ignite</bold> creativity,{" "}
+              <bold>build</bold> friendships and <bold>empower</bold> the next
+              generation of leaders in tech through building impactful
+              solutions.{" "}
+            </paragraph>
+            <br />
+            <br />
+            <br />
+            <br />
+            <button1>
+              <a href="/sign-up">Register Now</a>
+            </button1>
           </div>
         </div>
-        <div className = "leftJustified">
-        <paragraph>
-          <gradientFont>23%</gradientFont> <br/>
-          of Canadian workers in Science and Technology represent gender minorities. <br/>
-          <bold>YOU</bold> spark the change. Together, we can redefine the image of what it means to work in tech (hint: there are infinite possibilities).  
-        </paragraph>
+        <div className="leftJustified">
+          <paragraph>
+            <gradientFont>23%</gradientFont> <br />
+            of Canadian workers in Science and Technology represent gender
+            minorities. <br />
+            <bold>YOU</bold> spark the change. Together, we can redefine the
+            image of what it means to work in tech (hint: there are infinite
+            possibilities).
+          </paragraph>
         </div>
-        <div className = "centre">
-          <paragraph> Any Questions? <bold>Contact Us</bold></paragraph>
+        <div className="centre">
+          <paragraph>
+            {" "}
+            Any Questions? <bold>Contact Us</bold>
+          </paragraph>
         </div>
       </div>
       <Footer />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import SocialSideBar from "../components/SocialSideBar"
+import SocialSideBar from "../components/SocialSideBar";
 import register from "../imgs/registerText.svg";
 
 export default function SignUp() {
   return (
     <>
-        <NavBar />
-        <div className= "landingPage">
+      <NavBar />
+      <div className="regPageTitle">
+        <div>
           <img src={register} alt="register"></img>
-          <SocialSideBar />
         </div>
-        <div className = "centre">
-          <iframe src="https://ubc.ca1.qualtrics.com/jfe/form/SV_bO6taMrJnPF81kG" width="800px" height="2500" frameBorder="0"></iframe>
+        <div className="regPageSocials">
+        <SocialSideBar className="reg-icons" />
         </div>
-        
-        <Footer />
+      </div>
+      <div className="registerPageBody">
+        <iframe
+          title="qualtrics"
+          src="https://ubc.ca1.qualtrics.com/jfe/form/SV_bO6taMrJnPF81kG"
+          width="800px"
+          height="3000px"
+          frameBorder="0"
+        ></iframe>
+      </div>
+      <Footer />
     </>
   );
 }
-
