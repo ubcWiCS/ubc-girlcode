f946e9845df658bc6d6857926aff713d5939c3af
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2022-11-10 23:04:46 -0800

    Deploy 3 (#23)
    * bubbles and styling
    
    * Fixes (#18)
    
    * remove duplicate word
    
    * update links, footer cleanups
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
    
    * try mobile media query (#19)
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
    
    * Mobile (#20)
    
    * try mobile media query
    
    * form labels color
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
    
    * footer css
    
    * footer update
    
    * Mobile (#22)
    
    * try mobile media query
    
    * form labels color
    
    * fix mobile sizing
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
    
    Co-authored-by: LenaWang03 <xianna03@gmail.com>
    Co-authored-by: LenaWang03 <58739287+LenaWang03@users.noreply.github.com>
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa";
+import Typography from "@mui/material/Typography";
 
-import Typography from "../components/Typography";
+import { FaInstagram, FaFacebook, FaLinkedin, FaTiktok } from "react-icons/fa";
 
 import wics from "../imgs/wics.svg";
 import wids from "../imgs/wids.svg";
@@ -14,51 +14,64 @@ export default function Footer() {
       <div className="footer-body">
         <div className="footer-top-container">
           <img src={aritzia} alt="ubc girlcode" className="footer-logo"></img>
-          <Typography color="white">
-            Copyright © girlCode, 2022 | Code of Conduct | Privacy Policy
-          </Typography>
-        </div>
-        <div className="footer-right-container">
-          <span className="footer-clubs">
-            <img
-              src={ubc}
-              alt="ubc girlcode"
-              className="footer-logo-club"
-            ></img>
-            <img
-              src={wids}
-              alt="ubc girlcode"
-              className="footer-logo-club"
-            ></img>
-            <a href="https://ubcwics.com/">
-              <img
-                src={wics}
-                alt="ubc girlcode"
-                className="footer-logo-club"
-              ></img>
-            </a>
-          </span>
           <span>
             <a
-              href="https://www.instagram.com/girlcode.ubc/"
+              href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D"
               className="social-link"
             >
               <FaInstagram size={25} className="footer-logo-social" />
             </a>
             <a
-              href="https://www.facebook.com/events/1625780564822041"
+              href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D"
               className="social-link"
             >
               <FaFacebook size={25} className="footer-logo-social" />
             </a>
             <a
-              href="https://www.tiktok.com/@girlcode.ubc?_t=8XCbykWu7OM&_r=1"
+              href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D"
+              className="social-link"
+            >
+              <FaLinkedin size={25} className="footer-logo-social" />
+            </a>
+            <a
+              href="https://www.facebook.com/events/1625780564822041/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A1080145729316975%7D%7D]%22%7D"
               className="social-link"
             >
               <FaTiktok size={25} className="footer-logo-social" />
             </a>
           </span>
-          <Typography color="white">hello@girlcode.com</Typography>
+        </div>
+        <div className="footer-bottom-container">
+          <div className="footer-left-container">
+            <Typography
+              color="white"
+              component="p"
+              variant="caption"
+              gutterBottom={false}
+            >
+              Copyright © girlCode, 2022 | Code of Conduct | Privacy Policy
+            </Typography>
+          </div>
+          <div className="footer-right-container">
+            <span className="footer-clubs">
+              <img
+                src={ubc}
+                alt="ubc girlcode"
+                className="footer-logo-club"
+              ></img>
+              <img
+                src={wics}
+                alt="ubc girlcode"
+                className="footer-logo-club"
+              ></img>
+              <img
+                src={wids}
+                alt="ubc girlcode"
+                className="footer-logo-club"
+              ></img>
+            </span>
+            <Typography>hello@girlcode.com</Typography>
+          </div>
         </div>
       </div>
     </>
