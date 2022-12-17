b5f2f7e5fba889335e7f0ab89b676b3166cdced7
Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
2022-12-17 14:51:32 -0800

    add draft page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,9 +5,6 @@ import Footer from "../components/Footer";
 import SocialSideBar from "../components/SocialSideBar";
 import Typography from "../components/Typography";
 
-import bubblesLeft3 from "../imgs/bubbles-left-2.svg";
-import manyBubbles from "../imgs/many-bubbles-right.svg";
-import bubblesLeft2 from "../imgs/bubbles-left-3.svg";
 import bubblesRight from "../imgs/bubbles-right.svg";
 
 export default function SignUp() {
@@ -19,21 +16,23 @@ export default function SignUp() {
           <gradient-large>Register</gradient-large>
         </div>
         <SocialSideBar className="icons" />
-        
-        <img src={bubblesRight} alt="Aritzia" className="bubbles-right registerRight"></img>
+        <img
+          src={bubblesRight}
+          alt="Aritzia"
+          className="bubbles-right registerRight"
+        ></img>
       </div>
-      <div className = "centered-no-padding">
+      <div className="centered-no-padding">
         <div className="about-responsive page-container">
           <Typography color="white" weight="bold" variant="h1">
-              Registration is now closed. 
+            Registration is now closed.
           </Typography>
           <br></br>
           <Typography color="white">
-          Looking forward to seeing you in 2023!
+            Looking forward to seeing you in 2023!
           </Typography>
         </div>
       </div>
-      
       <Footer />
     </>
   );
