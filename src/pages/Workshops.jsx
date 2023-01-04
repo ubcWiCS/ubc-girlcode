574cade306617b5a083ff6c4c8188f2c607a199d
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2023-01-03 20:42:35 -0800

    Add Workshops Page (#51)
    * start workshops page
    
    * finalize workshop details
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

---
+++ src/pages/Workshops.jsx
@@ -0,0 +1,253 @@
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import SocialSideBar from "../components/SocialSideBar";
+import WorkshopModal from "../components/WorkshopModal";
+
+import bubblesLeft from "../imgs/bubbles-left-3.svg";
+import bubblesThree from "../imgs/ThreeRightBubbles.svg";
+
+const workshops = [
+  {
+    name: "Intro to Git and GitHub",
+    description:
+      "Get ready to collaborate on your coding projects! This is a workshop designed for beginners who have limited to no experience with Git and GitHub. We will teach to the key skills needed to work on shared projects with your hackathon teammates.",
+    url: "https://forms.gle/WiuXpGrBvxiXrs3m9",
+  },
+  {
+    name: "Intro to iOS",
+    description:
+      "Have an idea for a cool mobile app? This workshop will introduce you to creating an iOS app.",
+    url: "https://forms.gle/qHckdGwYcqs3PEuL7",
+  },
+  {
+    name: "Intro to Web Development",
+    description:
+      "Fresh out of 110 and have no idea how you will make a project with Dr. Racket? Have no fear, JavaScript is here to save you! This is a workshop designed for beginners. We will teach to the fundamentals of HTML, CSS, and JavaScript so you understand how basic web components are constructed.",
+    url: "https://forms.gle/5iJABZZRMuXjXW5g8",
+  },
+  {
+    name: "Succeeding in Your First Hackathon",
+    description:
+      "Is this your first Hackathon? No worries - we have tips and tricks from seasoned Hackathon participants to help guide you during the weekend.",
+    url: "https://forms.gle/v1ZcAu3rQ8nUyaSQ9",
+  },
+  {
+    name: "Intro to Python",
+    description:
+      "Python is a language commonly used language for quickly building apps, including those using machine learning tools. We will share some useful aspects of Python in this workshop.",
+    url: "https://forms.gle/66KxxkYASprnaf7F6",
+  },
+  {
+    name: "Intro to React",
+    description:
+      "It's time to test your new web development skills! This is a workshop designed for beginners, and a follow up to the Intro to Web Dev workshop. We will introduce you to the React framework and work through a coding exercise.",
+    url: "https://forms.gle/ac3qEUeXkeeYC6X58",
+  },
+  {
+    name: "Intro to Figma",
+    description:
+      "Figma is a great tool for mocking up your apps, and is used in industry by UI/UX design experts. We will cover some basic Figma concepts to help create your app design.",
+    url: "https://forms.gle/LCNu9yUkTsiz34MC9",
+  },
+];
+
+export default function Workshops() {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+
+  const handleOpenModal = (e) => {
+    if (show === true) {
+      return;
+    }
+
+    setName(workshops[e.target.value].name);
+    setDescription(workshops[e.target.value].description);
+    setURL(workshops[e.target.value].url);
+    setShow(true);
+  };
+
+  const handleCloseModal = () => {
+    setShow(false);
+  };
+
+  return (
+    <>
+      <NavBar />
+      {show ? (
+        <WorkshopModal
+          show={show}
+          setShow={handleCloseModal}
+          name={name}
+          description={description}
+          url={url}
+        />
+      ) : null}
+      <div className="page-starter-container">
+        <div>
+        <img
+            src={bubblesLeft}
+            alt="Aritzia"
+            className="bubbles-left"
+          ></img>
+          <img
+            src={bubblesThree}
+            alt="Aritzia"
+            className="home-starter-bubbles-right"
+          ></img>
+          <gradient-medium>Workshops</gradient-medium>
+        </div>
+        <SocialSideBar className="icons" />
+      </div>
+      <div className="page-container">
+        <Table striped bordered responsive variant="dark">
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Sunday, Jan 8</th>
+              <th>Monday, Jan 9</th>
+              <th>Tuesday, Jan 10</th>
+              <th>Wednesday, Jan 11</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+              <td>11 am</td>
+              <td>
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={3}
+                >
+                  Succeeding in Your First Hackathon
+                </button>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>12 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 pm</td>
+              <td>
+              {" "}
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={0}
+                >
+                Intro to Git and GitHub
+                </button>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>2 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>3 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>4 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>5 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>6 pm</td>
+              <td></td>
+              <td>
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={1}
+                >
+                  Intro to iOS
+                </button>
+              </td>
+              <td>
+              {" "}
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={5}
+                >
+                Intro to React
+                </button>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>6:30 pm</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+              {" "}
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={6}
+                >
+                Intro to Figma
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>8 pm</td>
+              <td></td>
+              <td>                {" "}
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={2}
+                >
+                Intro to Web Development
+                </button></td>
+              <td>
+              {" "}
+                <button
+                  className="workshop-btn"
+                  onClick={handleOpenModal}
+                  value={4}
+                >
+                Intro to Python
+                </button>
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <Footer />
+    </>
+  );
+}
