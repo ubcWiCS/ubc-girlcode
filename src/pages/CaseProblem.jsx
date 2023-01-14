faadfa02b2d6ed174adf6416dccde3c1f1f4a9b0
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2023-01-13 17:00:12 -0800

    Case (#64)
    * add hackathon workshops
    
    * add case problem
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

--- src/pages/CaseProblem.jsx
+++ src/pages/CaseProblem.jsx
@@ -15,9 +15,21 @@ export default function CaseProblem() {
         </div>
         <SocialSideBar className="icons" />
       </div>
-      <div className="about-responsive page-container title">
-        <Typography color="white" inline="inline" weight="bold" variant="h6">
-          TBA
+      <div className="contact-container leftJustified">
+        <Typography color="purple" weight="bold" variant="h3">
+          How can we use technology to personalize fashion-retail while
+          enhancing the e-commerce experience?
+        </Typography>
+        <Typography color="white" variant="h6">
+          Your job is to integrate your team's skills in data science, computer
+          science, design and product management to come up with a product that
+          can be used to forward the fashion-tech industry through the lens of
+          Aritzia. We encourage you to spend some time researching other brands,
+          latest e- commerce news and apps for inspiration, as well as Aritzia’s
+          current strategies, mission, and both their online and in-store
+          experiences. Think about current events in the tech world and how they
+          can be integrated to define what it means to have and maintain a
+          leading role in the fashion x tech world.
         </Typography>
       </div>
       <Footer />
